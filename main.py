3392538567c526cb4e12ff6b04da60b1a9ee8ab8
Anurag Karki <69763071+Anurag9841@users.noreply.github.com>
2022-01-21 18:53:02 +0545

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+# initialize pygame
 pygame.init()
 # creating screen
 screen = pygame.display.set_mode((1000, 800))
@@ -8,24 +9,42 @@ screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption('Space Inveador')
 icon = pygame.image.load('Ufo.png')
 pygame.display.set_icon(icon)
-background=pygame.image.load('background1.jpg')
-# player
+# background_image
+background = pygame.image.load('background1.jpg')
+# player_image
 player_img = pygame.image.load('spaceship.png')
+# enemy_image
 enemy_img = pygame.image.load('enemy.png')
 enemyX = random.randint(0, 936)
 enemyY = random.randint(50, 150)
+enemyX_change = 0.6
+enemyY_change = 20
 playerX = 450
 playerY = 600
 playerX_change = 0
 playerY_change = 0
-enemyX_change = 0.6
-enemyY_change = 40
+
+# bullet_image
+bullet_img = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 600
+bullet_change = 0
+bulletY_change = 2
+bullet_state = "ready"  # ready--> it is the state where u cannot see the bullet at the screen
+
+
 def enemy(x, y):
     screen.blit(enemy_img, (x, y))
 
 
 def player(x, y):
     screen.blit(player_img, (x, y))
+
+
+def bullet(X, Y):
+    global bullet_state
+    bullet_state = "fire"  # fire--->bullet is moving
+    screen.blit(bullet_img, (X + 15, Y + 10))
 
 
 # game loop
@@ -45,6 +64,10 @@ while run:
                 playerY_change = -0.6
             if event.key == pygame.K_DOWN:
                 playerY_change = 0.6
+            if event.key == pygame.K_SPACE:
+                if bullet_state is "ready":
+                    bulletX = playerX
+                    bullet(bulletX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                 playerX_change = 0
@@ -63,18 +86,24 @@ while run:
     if playerY >= 730:
         playerY = 730
 
-    enemyX += enemyX_change
     if enemyX <= 0:
         enemyX_change = 0.6
-        enemyY+=enemyY_change
+        enemyY += enemyY_change
     if enemyX >= 936:
         enemyX_change = -0.6
         enemyY += enemyY_change
+    enemyX += enemyX_change
     # enemyY += enemyY_change
     # if enemyY <= 0:
     #     enemyY = 0
     # if enemyY >= 730:
     #     enemyY = 730
+    if bullet_state is "fire":
+        bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+        if bulletY <= 0:
+            bulletY = playerY
+            bullet_state = "ready"
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
